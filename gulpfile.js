d14a322682074d5b0fff9b68ce388de734ac6abd
Raphael Traviss <raphael@skyleafdesign.com>
2018-05-30 14:21:37 -0500

    Gulp now automatically rebuilds Node addon.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const exec = require('child_process').exec;
 const log = require('fancy-log');
 
 const clang_build = "clang src/main.c -o dist/liblhvm";
+const gyp_build = "node-gyp build";
 
 function build(done) {
   exec(clang_build, function (error, stdout, stderr) {
@@ -13,8 +14,19 @@ function build(done) {
   });
 }
 
-function watch() {
-  return gulp.watch('src/*.c', build);
+function gyp(done) {
+  exec(gyp_build, function (error, stdout, stderr) {
+    log("Built the Node addon.");
+    if (stdout.length > 0) log(stdout);
+    if (stderr.length > 0) log(stderr);
+    done();
+  });
+}
+
+function watch(done) {
+  gulp.watch('src/*.c', build);
+  gulp.watch('node_wrapper.cpp', gyp);
+  done();
 };
 
-gulp.task('default', gulp.series([ build, watch ]));
+gulp.task('default', gulp.series([ build, gyp, watch ]));
