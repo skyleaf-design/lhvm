d80e2ed91d8a15faaa0c6d4c3de8f9e41e393f62
Raphael Traviss <raphael@skyleafdesign.com>
2018-05-31 13:44:35 -0500

    Beginning to process JS args in C++.

--- node_wrapper.cpp
+++ node_wrapper.cpp
@@ -7,6 +7,8 @@ namespace liblhvm {
 // buffer must be string lenght + 2 for null character and newline.
 char buffer[10];
 
+
+
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
@@ -19,7 +21,35 @@ void Method(const FunctionCallbackInfo<Value>& args) {
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
 }
 
-void BoingWrapper(const FunctionCallbackInfo<Value>& args) {
+void ReturnString(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  boing(buffer, 5);
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, buffer));
+}
+
+void Echo(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  auto x = args.Data();
+  if (x->IsBoolean()) {
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "is_boolean"));
+    return;
+  }
+  if (x->IsString()) {
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "is_string"));
+    return;
+  }
+  if (x->IsObject()) {
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "is_object"));
+    return;
+  }
+  if (x->IsUndefined()) {
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "is_undefined"));
+    return;
+  }
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "is_unknown"));
+}
+
+void ReturnArrayOfStrings(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
   boing(buffer, 5);
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, buffer));
@@ -27,7 +57,9 @@ void BoingWrapper(const FunctionCallbackInfo<Value>& args) {
 
 void init(Local<Object> exports) {
   NODE_SET_METHOD(exports, "hello", Method);
-  NODE_SET_METHOD(exports, "boing", BoingWrapper);
+  NODE_SET_METHOD(exports, "string", ReturnString);
+  NODE_SET_METHOD(exports, "echo", Echo);
+  NODE_SET_METHOD(exports, "array_of_strings", ReturnArrayOfStrings);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, init)
--- tests.js
+++ tests.js
@@ -4,4 +4,6 @@ const addon = require('./build/Release/lhvm.node');
 // Using the wrapper, pass liblhvm some NumberForth strings, inspect the
 // results, and then pass or fail the tests depending on the result.
 console.log(addon.hello());
-console.log(addon.boing());
+console.log(addon.string());
+console.log(addon.echo("not true"));
+console.log(addon.echo(true));
